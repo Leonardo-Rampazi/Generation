4e1eb1e2dece07a127c352e744aa3519ac6374fe
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-06-07 15:21:02 -0300

    Colocando todos os exercicios

---
+++ Java/src/br/com/generation/Exercicios02/src/Exercicios02Condicionais/Ex03Condicionais.java
@@ -0,0 +1,23 @@
+package Exercicios02Condicionais;/*3- Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
+categoria ela se encontra:
+ 10-14 infantil
+ 15-17 juvenil
+ 18-25 adulto*/
+import javax.swing.JOptionPane;
+public class Ex03Condicionais {
+
+	public static void main(String[] args) {
+		int idade;
+		idade= Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: "));
+		if (idade>=10&&idade<=14) {
+			JOptionPane.showMessageDialog(null,"Sua idade é: "+idade+" Você ta na classe Infantil");
+		}else if (idade>=15&&idade<=17) {
+			JOptionPane.showMessageDialog(null,"Sua idade é: "+idade+" Você ta na classe Juvenil");
+		}else if (idade >=18&&idade<=25) {
+			JOptionPane.showMessageDialog(null,"Sua idade é: "+idade+" Você ta na classe Adulta");
+		}else {
+			JOptionPane.showMessageDialog(null,"Sua idade é: "+idade+" não Pode fazer parte!!");
+		}
+	}
+
+}
---
+++ Java/src/br/com/generation/Exercicios02/src/Exercicios02Condicionais/Ex04Condicionais.java
@@ -0,0 +1,22 @@
+package Exercicios02Condicionais;/*4- Faça um programa em que permita a entrada de um número qualquer e exiba se este
+número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
+ímpar exiba o número elevado ao quadrado.*/
+import javax.swing.JOptionPane;
+public class Ex04Condicionais {
+
+	public static void main(String[] args) {
+		double numero,resultado;
+		
+		numero= Integer.parseInt(JOptionPane.showInputDialog("Digite um numero qualquer: "));
+		if (numero%2==0) {
+			JOptionPane.showMessageDialog(null,"Seu numero é par: "+numero);
+			resultado = Math.sqrt(numero);
+			JOptionPane.showMessageDialog(null,"A Raiz quadrada é: "+resultado);
+		}else {
+			JOptionPane.showMessageDialog(null,"Seu numero é impar: "+numero);
+			resultado = (numero*numero);
+			JOptionPane.showMessageDialog(null,"O numero elevado ao quadrado é: "+resultado);
+		}
+	}
+
+}
