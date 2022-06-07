6d94d1cb09076e01ee632083853420ddeda51c05
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-06-07 13:59:32 -0300

    Ajustando o ex02

--- Java/src/br/com/generation/Exercicios02/src/Exercicios02Condicionais/Ex02Condicionais.java
+++ Java/src/br/com/generation/Exercicios02/src/Exercicios02Condicionais/Ex02Condicionais.java
@@ -8,27 +8,27 @@ public class Ex02Condicionais {
 		n2= Integer.parseInt(JOptionPane.showInputDialog("Digite um numero inteiro: "));
 		n3= Integer.parseInt(JOptionPane.showInputDialog("Digite um numero inteiro: "));
 		  
-		if (n1 <= n2 && n2 <= n3)
+		if (n1 >= n2 && n2 >= n3)
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero1: "+n1+"Numero2: "+n2+"Numero3: "+n3);
 		    }
-		  else if (n1 <= n3 && n3 <= n2)
+		  else if (n1 >= n3 && n3 >= n2)
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero1: "+n1+"Numero3: "+n3+"Numero2: "+n2);
 		    }
-		  else if (n2 <= n1 && n1 <= n3)
+		  else if (n2 >= n1 && n1 >= n3)
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero2: "+n2+"Numero1: "+n1+"Numero3: "+n3);
 		    }
-		  else if (n2 <= n3 && n3 <= n1) 
+		  else if (n2 >= n3 && n3 >= n1) 
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero2: "+n2+"Numero3: "+n3+"Numero1: "+n1);
 		    }
-		  else if (n3 <= n1 && n1 <= n2) 
+		  else if (n3 >= n1 && n1 >= n2) 
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero3: "+n3+"Numero1: "+n1+"Numero2: "+n2);
 		    }
-		  else /* n3 <= n2 && n2 < n1 */
+		  else /* n3 >= n2 && n2 > n1 */
 		    {
 			  JOptionPane.showMessageDialog(null,"A ordem crescente Numero3: "+n3+"Numero2: "+n2+"Numero1: "+n1);
 		    }
