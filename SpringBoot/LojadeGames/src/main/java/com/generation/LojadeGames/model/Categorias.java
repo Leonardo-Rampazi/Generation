36c01d622473c889a231d9f6d666ce15e1bb2f78
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-07-06 17:07:25 -0300

    colocando o exercicio da loja de games

---
+++ SpringBoot/LojadeGames/src/main/java/com/generation/LojadeGames/model/Categorias.java
@@ -0,0 +1,87 @@
+package com.generation.LojadeGames.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categorias {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5,max =200,message="no minimo 5 letras e no maximo 200 letras")
+	private String tipo;
+	
+	@NotNull
+	@Size(min = 5,max =200,message="no minimo 5 letras e no maximo 200 letras")
+	private String classe;
+	
+	@NotNull
+	@Size(min = 5,max =400,message="no minimo 5 letras e no maximo 400 letras")
+	private String descricao;
+	
+	@NotNull
+	private int quantidade;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro = new java.sql.Date(System.currentTimeMillis());
+	
+	@OneToMany(mappedBy="categorias", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categorias")
+	private List<Produtos> produtos;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getClasse() {
+		return classe;
+	}
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	};
+	
+}
--- SpringBoot/LojadeGames/src/main/java/com/generation/LojadeGames/repository/CategoriasRepository.java
+++ SpringBoot/LojadeGames/src/main/java/com/generation/LojadeGames/repository/CategoriasRepository.java
@@ -1,16 +1,14 @@
-package com.generation.farmacia.repository;
+package com.generation.LojadeGames.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.generation.farmacia.model.Categorias;
-
+import com.generation.LojadeGames.model.Categorias;
 
 @Repository
 public interface CategoriasRepository extends JpaRepository<Categorias, Long>{
-	public List<Categorias> findAllByDescricaoContainingIgnoreCase(String tipo);
+	public List<Categorias> findAllByTipoContainingIgnoreCase(String tipo);
+
 }
-
-
